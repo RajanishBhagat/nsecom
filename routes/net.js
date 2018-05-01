06ac89f828a54b633806c6b4891ccf13ffc4013c
root <root@info@cavisson.com>
2018-05-01 20:04:13 +0530

    Second commit

--- routes/net.js
+++ routes/net.js
@@ -1,49 +1,82 @@
 /**
- * Created by netstorm on 4/6/17.
+ * Created by Sahil on 19-10-2015.
  */
-var net = require('net')
-var app = require('express')
-var netrouter = app.Router()
 
+var express = require('express');
+var router = express.Router();
+var http = require('http');
+var pg = require('pg');
 
-function createNetConnection(req, res){
-    var server = net.createServer(function(connection) {
-        console.log('client connected');
+//var connection= require('cassandra-client').Connection;
 
-        connection.on('end', function() {
-            console.log('client disconnected');
-            server.close();
-        });
-        connection.write('Hello World!\r\n');
-    });
-    server.listen(8080, function() {
-        console.log('server is listening');
-    });
-    netCalloutResp(res)
+var f1 = false;
+var f2 = false;
+//var f3 = false;
+
+function x(){
+    throw new Error("Custom Error");
 }
 
-function netCalloutResp(res) {
-    res.render('mongodb', {DB: 'NET !'});
+function multiHttpConnection(req, res) {
+    try {
+        var options = {
+            host: 'www.google.com'
+        };
+
+        var callback = function (response) {
+            var str = '';
+
+            //another chunk of data has been recieved, so append it to `str`
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
+
+            //the whole response has been recieved, so we just print it out here
+            response.on('end', function () {
+                    sendResp();
+            });
+        };
+
+        var options1 = {
+            host: 'www.gmail.com',
+            headers : {
+                'Connection' : 'keep-alive'
+            }
+        };
+        callback1 = function (response) {
+            var str = '';
+
+            //another chunk of data has been recieved, so append it to `str`
+            response.on('data', function (chunk) {
+            });
+
+            //the whole response has been recieved, so we just print it out here
+            response.on('end', function () {
+                sendResp();
+
+            });
+        };
+        x();
+        http.request(options, callback).on('error',function(err){console.log(err)}).end();
+        //sendResp();
+    }
+    catch(err)
+    {
+        console.log("error in making http calls : "+err)
+        sendResp();
+    }
+
+
+
+    function sendResp(){
+        res.render('checkout');
+    };
 
 }
-function createNetConnectionforClient(req,res)
-{
-    var client = net.connect({port: 8080}, function() {
-        console.log('connected to server!');
-    });
-    client.on('data', function(data) {
-        console.log(data.toString());
-        client.end();
-    });
-    client.on('end', function() {
-        console.log('disconnected from server');
-    });
-}
-/ GET home page. /
-netrouter.get('/', function(req, res)
-{
-    createNetConnection(req, res);
-    createNetConnectionforClient(req,res)
+
+router.get('/', function(req, res, next){
+    multiHttpConnection(req, res);
 });
 
-module.exports = netrouter;
+
+module.exports = router;
